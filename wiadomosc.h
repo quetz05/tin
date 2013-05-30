42f1c671627a84d2bdacb25e422d361bbbf56ea2
quetz05 <quetz@quetz-K50ID.(none)>
2013-05-30 22:24:24 +0200

    Dodanie wiadomosc.h i zrobienie okna rejestracji

---
+++ wiadomosc.h
@@ -0,0 +1,28 @@
+﻿#ifndef WIADOMOSC_H
+#define WIADOMOSC_H
+
+class Naglowek
+{
+    public:
+        int typ;
+        int ID;
+        int rozmiar;
+};
+
+
+class Wiadomosc
+{
+
+    public:
+
+        Wiadomosc() {ilosc = 1024;}
+        Wiadomosc(int x) {ilosc = x;}
+        Naglowek naglowek;
+
+    private:
+        int ilosc;
+        char dane[ilosc];
+
+};
+
+#endif // WIADOMOSC_H
