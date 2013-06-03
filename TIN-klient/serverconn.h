fd2f604edbb521fb557e41e95a8c062bcf0a3813
quetz05 <quetz05@gmail.com>
2013-06-03 22:40:27 +0200

    Update serverconn.h

--- TIN-klient/serverconn.h
+++ TIN-klient/serverconn.h
@@ -9,6 +9,12 @@ class ServerConn: public QObject
 {
     Q_OBJECT
 
+/**
+ * @brief The ServerConn class
+ * Dzialajaca w odrebnym watku odpowiada za odbieranie wiadomosci z serwera i kierowanie
+ * ich do odpowiednich klas
+*
+*/
 public:
     explicit ServerConn(QObject *parent = 0, int socket = 0);
     void doSetup(QThread *cThread);
