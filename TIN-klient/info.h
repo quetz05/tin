88c9af2371f36e789d22e54497a82c04333cac80
quetz05 <quetz05@gmail.com>
2013-06-03 22:28:36 +0200

    Update info.h

--- TIN-klient/info.h
+++ TIN-klient/info.h
@@ -7,11 +7,20 @@ namespace Ui {
 class info;
 }
 
+ /**
+    * @brief The info class
+    * Klasa okna informacyjnego
+    */
 class info : public QDialog
 {
     Q_OBJECT
     
 public:
+ /**
+    * @brief info
+    * Konstruktor budujacy okno o danej wiadomosci i zaleznie od wartosci ok - okreslajacej czy informacja
+    * jest pozytywna czy negatywna
+    */
     explicit info(QWidget *parent = 0, QString info = "", bool ok = false);
     ~info();
 
